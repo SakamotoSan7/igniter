dda338cbe220ea4db0f0c1142c8be60a44809550
Kristianto <inikristiantoitu@gmail.com>
2024-12-18 14:07:09 +0700

    configuring localFont workSans and installing tailwindcss-animate and typography

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
 		<header className="px-5 py-3 bg-white shadow-sm font-work-sans">
 			<nav className="flex items-center justify-between">
 				<Link href="/">
-					<Image src="/logo.png" alt="logo " width={144} height={30} />
+					<Image src="/logo.png" alt="logo" width={144} height={30} />
 				</Link>
 				<div className="flex items-center gap-5 text-black">
 					{session && session?.user ? (
@@ -22,7 +22,7 @@ const Navbar = async () => {
 							<form
 								action={async () => {
 									"use server";
-									await signOut({redirectTo: "/"});
+									await signOut({ redirectTo: "/" });
 								}}
 							>
 								<button type="submit">Logout</button>
