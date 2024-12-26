030dd4e1b82e8360ebc9d195ca247ad2c8ac12c6
Kristianto <inikristiantoitu@gmail.com>
2024-12-26 11:37:55 +0700

    add Sanity type generation and startup queries; update StartupCard component for author handling; and install npm i server-only

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,38 +1,53 @@
-import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import SearchForm from '@/components/SearchForm';
+import StartupCard, { StartupCardType } from '@/components/StartupCard';
+import { client } from '@/sanity/lib/client';
+import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query: string }> }) {
 	const query = (await searchParams).query;
 
-	const posts = [
-		{
-			_createdAt: new Date(),
-			views: 55,
-			author: { _id: 1, name: "Kristianto" },
-			_id: 1,
-			description: "This is a description",
-			image: "https://eu-images.contentstack.com/v3/assets/blt31d6b0704ba96e9d/blt4d3f4826b393a3c8/6709363cc05f7e768f4f24a0/Tesla_Optimus_Humanoid_Robot_Serves_Drinks.png?width=1280&auto=webp&quality=95&format=jpg&disable=upscale",
-			category: "Robots",
-			title: "We Robots",
-		},
-	];
+	const posts = await client.fetch(STARTUPS_QUERY);
+
+	// const posts = [
+	// 	{
+	// 		_createdAt: new Date(),
+	// 		views: 55,
+	// 		author: { _id: 1, name: "Kristianto" },
+	// 		_id: 1,
+	// 		description: "This is a description",
+	// 		image: "https://eu-images.contentstack.com/v3/assets/blt31d6b0704ba96e9d/blt4d3f4826b393a3c8/6709363cc05f7e768f4f24a0/Tesla_Optimus_Humanoid_Robot_Serves_Drinks.png?width=1280&auto=webp&quality=95&format=jpg&disable=upscale",
+	// 		category: "Robots",
+	// 		title: "We Robots",
+	// 	},
+	// ];
 
 	return (
 		<>
-			<section className="pink_container">
-				<h1 className="heading">
+			<section className='pink_container'>
+				<h1 className='heading'>
 					Pitch Your Startup <br /> Connect With Entrepreneurs
 				</h1>
 
-				<p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+				<p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
 
 				<SearchForm query={query} />
 			</section>
 
-			<section className="section_container">
-				<p className="text-30-semibold">{query ? `Search Results for "${query}"` : "All Startups"}</p>
+			<section className='section_container'>
+				<p className='text-30-semibold'>{query ? `Search Results for "${query}"` : 'All Startups'}</p>
 
-				<ul className="mt-7 card_grid">{posts?.length > 0 ? posts.map((post: StartupCardType) => <StartupCard key={post?.id} post={post} />) : <p className="no-results">No Startups Found</p>}</ul>
+				<ul className='mt-7 card_grid'>
+					{posts?.length > 0 ? (
+						posts.map((post: StartupCardType) => (
+							<StartupCard
+								key={post?._id}
+								post={post}
+							/>
+						))
+					) : (
+						<p className='no-results'>No Startups Found</p>
+					)}
+				</ul>
 			</section>
 		</>
 	);
--- src/components/StartupCard.tsx
+++ src/components/StartupCard.tsx
@@ -5,18 +5,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { Button } from './ui/button';
+import { Author, Startup } from '@/sanity/types';
+
+export type StartupCardType = Omit<Startup, 'author'> & {
+	author?: Author;
+};
 
 const StartupCard = ({ post }: { post: StartupCardType }) => {
-	const {
-		_createdAt,
-		views,
-		author: { _id: authorId, name },
-		_id,
-		description,
-		image,
-		category,
-		title,
-	} = post;
+	const { _createdAt, views, author, _id, description, image, category, title } = post;
 
 	return (
 		<li className='startup-card group'>
@@ -32,8 +28,8 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
 
 			<div className='flex-between mt-5 gap-5'>
 				<div className='flex-1'>
-					<Link href={`/user/${authorId}`}>
-						<p className='text-16-medium line-clamp-1'>{name}</p>
+					<Link href={`/user/${author?._id}`}>
+						<p className='text-16-medium line-clamp-1'>{author?.name}</p>
 					</Link>
 
 					<Link href={`/startup/${_id}`}>
@@ -42,8 +38,14 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
 				</div>
 
 				<div>
-					<Link href={`/user/${authorId}`}>
-						<Image src={'https://placehold.co/48x48'} alt='placeholder' width={48} height={48} className='rounded-full' />
+					<Link href={`/user/${author?._id}`}>
+						<Image
+							src={'https://placehold.co/48x48'}
+							alt='placeholder'
+							width={48}
+							height={48}
+							className='rounded-full'
+						/>
 					</Link>
 				</div>
 			</div>
@@ -51,15 +53,22 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
 			<Link href={`/startup/${_id}`}>
 				<p className='startup-card_desc'>{description}</p>
 
-				<img src={image} alt='placeholder' className='startup-card_img' />
+				<img
+					src={image}
+					alt='placeholder'
+					className='startup-card_img'
+				/>
 			</Link>
 
 			<div className='flex-between gap-3 mt-5'>
-				<Link href={`/query=${category.toLowerCase()}`}>
+				<Link href={`/query=${category?.toLowerCase()}`}>
 					<p className='text-16-medium'>{category}</p>
 				</Link>
 
-				<Button className='startup-card_btn' asChild>
+				<Button
+					className='startup-card_btn'
+					asChild
+				>
 					<Link href={`/startup/${_id}`}>Details</Link>
 				</Button>
 			</div>
